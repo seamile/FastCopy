be2f272fcdff96866eb55dd32856f00ff531f1a0
Seamile <lanhuermao@gmail.com>
2021-08-14 21:07:20 +0800

    release 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f_readme:
 
 setuptools.setup(
     name="fastcopy",
-    version="0.1.4",
+    version="0.1.5",
     python_requires=">=3.6",
     author="Seamile",
     author_email="lanhuermao@gmail.com",
